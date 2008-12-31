817da17972c2929266fdda01b06c4cea3ce7abdf
Stefan Sundin <stefansundin@users.noreply.github.com>
2008-12-31 19:40:20 +0000

    Adding Spanish translation from Fabrizio Ferri. Thanks!

---
+++ localization/es-ES/strings.h
@@ -0,0 +1,42 @@
+/*
+	KillKeys - es-ES localization by Fabrizio Ferri (algernon@gmail.com)
+	Do not localize APP_NAME, it will be automatically replaced.
+	Keep this file in UTF–8
+	
+	Copyright (C) 2008  Stefan Sundin (recover89@gmail.com)
+	
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+*/
+
+//This will be updated when the strings in the program changes
+#define L10N_VERSION 1
+
+//Strings
+#define L10N_MENU_ENABLE       L"Activar"
+#define L10N_MENU_DISABLE      L"Desactivar"
+#define L10N_MENU_HIDE         L"Ocultar icono"
+#define L10N_MENU_AUTOSTART    L"Iniciar con Windows"
+#define L10N_MENU_UPDATE       L"Actualización disponible"
+#define L10N_MENU_ABOUT        L"Acerca de"
+#define L10N_MENU_EXIT         L"Salir"
+#define L10N_TRAY_ENABLED      APP_NAME " (activado)"
+#define L10N_TRAY_DISABLED     APP_NAME " (desactivado)"
+#define L10N_UPDATE_BALLOON    L"Nueva versión disponible"
+#define L10N_UPDATE_DIALOG     L"Hay una nueva versión disponible.\n¿Ir a la página web?"
+#define L10N_ABOUT_KEYS1       L"Teclas desactivadas sin estar en modo de pantalla completa:"
+#define L10N_ABOUT_KEYS2       L"Teclas desactivas estando en modo de pantalla completa:"
+#define L10N_ABOUT_KEYS3       L"Activa "APP_NAME" para ver qué teclas serán bloqueadas."
+#define L10N_ABOUT_TITLE       L"Acerca de " APP_NAME
+#define L10N_ABOUT             APP_NAME " - " APP_VERSION "\n\
+Por Stefan Sundin\n\
+Traducido por Fabrizio Ferri (algernon@gmail.com)\n\
+\n\
+Una vez activado, las teclas especificadas en el fichero KillKeys.ini serán desactivadas.\n\
+Por defecto, la teclá Windows y la tecla Menú están desactivadas en pantalla completa.\n\
+%s\n\
+\n\
+Visita la página web en "APP_URL"\n\
+Envía comentarios a recover89@gmail.com"
