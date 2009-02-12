51f777787f38384c245d29aa478fe6157ed66868
Stefan Sundin <stefansundin@users.noreply.github.com>
2009-02-12 19:07:08 +0000

    Updated codebase (installer, build script).

--- build.bat
+++ build.bat
@@ -6,37 +6,41 @@ if not exist build (
 	mkdir build
 )
 
-windres -o build/resources.o resources.rc
+windres -o build\resources.o resources.rc
 
 if "%1" == "all" (
+	gcc -o build\ini.exe ini.c -lshlwapi
+	
 	@echo.
 	echo Building binaries
-	if not exist "build/en-US/KillKeys" (
+	if not exist "build\en-US\KillKeys" (
 		mkdir "build\en-US\KillKeys"
 	)
-	gcc -o "build/en-US/KillKeys/KillKeys.exe" killkeys.c build/resources.o -mwindows -lshlwapi -lwininet
-	if exist "build/en-US/KillKeys/KillKeys.exe" (
-		strip "build/en-US/KillKeys/KillKeys.exe"
+	gcc -o "build\en-US\KillKeys\KillKeys.exe" killkeys.c build\resources.o -mwindows -lshlwapi -lwininet
+	if not exist "build\en-US\KillKeys\KillKeys.exe" (
+		exit /b
 	)
+	strip "build\en-US\KillKeys\KillKeys.exe"
 	
 	for /D %%f in (localization/*) do (
 		@echo.
 		echo Putting together %%f
 		if not %%f == en-US (
-			if not exist "build/%%f/KillKeys" (
+			if not exist "build\%%f\KillKeys" (
 				mkdir "build\%%f\KillKeys"
 			)
-			copy "build\en-US\KillKeys\KillKeys.exe" "build/%%f/KillKeys"
+			copy "build\en-US\KillKeys\KillKeys.exe" "build\%%f\KillKeys"
 		)
-		copy "localization\%%f\info.txt" "build/%%f/KillKeys"
-		copy "KillKeys.ini" "build/%%f/KillKeys"
+		copy "localization\%%f\info.txt" "build\%%f\KillKeys"
+		copy "KillKeys.ini" "build\%%f\KillKeys"
+		"build\ini.exe" "build\%%f\SuperF4\SuperF4.ini" SuperF4 Language %%f
 	)
 	
 	@echo.
 	echo Building installer
 	makensis /V2 installer.nsi
 ) else (
-	gcc -o KillKeys.exe killkeys.c build/resources.o -mwindows -lshlwapi -lwininet -DDEBUG
+	gcc -o KillKeys.exe killkeys.c build\resources.o -mwindows -lshlwapi -lwininet -DDEBUG
 	
 	if "%1" == "run" (
 		start KillKeys.exe
