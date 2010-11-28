0233fcf3c89fed7500df80071405a92c6e93885f
Alexander De Sousa Macedo <xandy.lua@gmail.com>
2010-11-28 20:11:48 +0000

    Updated Galician and Spanish translation targeting 1.2 release.

--- localization/gl-ES/strings.h
+++ localization/gl-ES/strings.h
@@ -15,7 +15,7 @@ struct strings gl_ES = {
  /* menu_autostart */ L"Arrancar ó inicio",
  /* menu_settings  */ L"Abrir configuración",
  /* menu_chkupdate */ L"Comprobar actualizacións",
- /* menu_update    */ L"Actualización dispoñíbel",
+ /* menu_update    */ L"Actualización dispoñíbel!",
  /* menu_about     */ L"Acerca de",
  /* menu_exit      */ L"Saír",
  /* tray_enabled   */ APP_NAME " (activado)",
