101cd8c718dc22cd25463cc28ccf012fba07297c
Stefan Sundin <stefansundin@users.noreply.github.com>
2010-12-12 13:43:34 +0000

    Middle mouse button now opens KillKeys.ini.

--- killkeys.c
+++ killkeys.c
@@ -91,21 +91,9 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInstance, LPSTR szCmdLine, in
 	}
 	
 	//Create window
-	WNDCLASSEX wnd;
-	wnd.cbSize = sizeof(WNDCLASSEX);
-	wnd.style = 0;
-	wnd.lpfnWndProc = WindowProc;
-	wnd.cbClsExtra = 0;
-	wnd.cbWndExtra = 0;
-	wnd.hInstance = hInst;
-	wnd.hIcon = NULL;
-	wnd.hIconSm = NULL;
-	wnd.hCursor = LoadImage(NULL, IDC_ARROW, IMAGE_CURSOR, 0, 0, LR_DEFAULTCOLOR|LR_SHARED);
-	wnd.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
-	wnd.lpszMenuName = NULL;
-	wnd.lpszClassName = APP_NAME;
+	WNDCLASSEX wnd = {sizeof(WNDCLASSEX), 0, WindowProc, 0, 0, hInst, NULL, NULL, NULL, NULL, APP_NAME, NULL};
 	RegisterClassEx(&wnd);
-	g_hwnd = CreateWindowEx(0, wnd.lpszClassName, APP_NAME, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, HWND_MESSAGE, NULL, hInst, NULL);
+	g_hwnd = CreateWindowEx(0, wnd.lpszClassName, APP_NAME, 0, 0, 0, 0, 0, HWND_MESSAGE, NULL, hInst, NULL);
 	
 	//Load settings
 	wchar_t path[MAX_PATH];
@@ -246,7 +234,7 @@ LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
 		}
 	}
 	
-    return CallNextHookEx(NULL, nCode, wParam, lParam); 
+	return CallNextHookEx(NULL, nCode, wParam, lParam); 
 }
 
 int HookKeyboard() {
@@ -311,6 +299,9 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 	if (msg == WM_TRAY) {
 		if (lParam == WM_LBUTTONDOWN || lParam == WM_LBUTTONDBLCLK) {
 			ToggleState();
+		}
+		else if (lParam == WM_MBUTTONDOWN) {
+			SendMessage(hwnd, WM_COMMAND, SWM_SETTINGS, 0);
 		}
 		else if (lParam == WM_RBUTTONDOWN) {
 			ShowContextMenu(hwnd);
